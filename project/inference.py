abdafcbbd036b43f57baf1370d5e9f59d37bb8dd
Socret360 <lee.socret@gmail.com>
2023-04-14 22:44:43 +0100

    added inference.py for custom input

--- project/inference.py
+++ project/inference.py
@@ -16,6 +16,9 @@ Author: Socretquuliqaa <lee.socret@gmail.com>
 Created: 09/04/2023
 """
 
+import os
+import json
+import pickle
 import argparse
 import pandas as pd
 
@@ -26,7 +29,29 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Run inference from user input.')
     parser.add_argument('input', type=str, help='path to input csv file.')
-    parser.add_argument('model_path', type=str, help='path to saved model.')
+    parser.add_argument('model_path', type=str, help='path to saved model folder that contains both the pickle and json files.')
     parser.add_argument('--output_dir', type=str,
                         help='path to output directory.', default="output")
     args = parser.parse_args()
+
+    with open(os.path.join(args.model_path, "model.json"), 'r') as config_file:
+        config = json.load(config_file)
+
+    input_data = pd.read_csv(args.input)
+    input_data = input_data[['accel_x', 'accel_y', 'accel_z', 'skin_temp', 'heart_rate', 'blood_volume_pulse', 'eda', 'inter_beat_interval']]
+
+    X, _ = utils.preprocess(
+        input_data,
+        None,
+        window_size=config['window_size'],
+        stride=config['stride'],
+    )
+
+    clf = pickle.load(open(os.path.join(args.model_path, "model.pkl"), 'rb'))
+    y_pred = clf.predict(X)
+
+    # save the prediction to a csv file.
+    pd.DataFrame(
+        columns=['Stress'],
+        data=y_pred
+    ).to_csv(os.path.join(args.output_dir, "pred.csv"), index=False)
